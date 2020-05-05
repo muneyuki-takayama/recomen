574e1376bac60e4dd3d26111638fba02010dd07a
muneyukitakayama <muneyukitakayama@gmnail.com>
2020-05-05 23:22:27 +0900

    アップする画像の枚数を選択できるように調整

--- laravel/app/Http/Controllers/ProductController.php
+++ laravel/app/Http/Controllers/ProductController.php
@@ -30,14 +30,20 @@ class ProductController extends Controller
         $path1 = Storage::disk('s3')->putFile('recomen', $image_1, 'public');
         $product->pic1 = Storage::disk('s3')->url($path1);
 
-        $image_2 = $request->file('pic2');
-        $path2 = Storage::disk('s3')->putFile('recomen', $image_2, 'public');
-        $product->pic2 = Storage::disk('s3')->url($path2);
+        if(($request->file('pic2')))
+        {
+            $image_2 = $request->file('pic2');
+            $path2 = Storage::disk('s3')->putFile('recomen', $image_2, 'public');
+            $product->pic2 = Storage::disk('s3')->url($path2);
+        }
 
-        $image_3 = $request->file('pic3');
-        $path3 = Storage::disk('s3')->putFile('recomen', $image_3, 'public');
-        $product->pic3 = Storage::disk('s3')->url($path3);
-
+        if(($request->file('pic3')))
+        {
+            $image_3 = $request->file('pic3');
+            $path3 = Storage::disk('s3')->putFile('recomen', $image_3, 'public');
+            $product->pic3 = Storage::disk('s3')->url($path3);
+        }
+        
         $product->save();
 
         return redirect()->route('products.index');
