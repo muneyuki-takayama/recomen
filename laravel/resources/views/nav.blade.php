b767b91d07fcc688f0ee524a37bdf7ba0c4a112b
muneyukitakayama <muneyukitakayama@gmnail.com>
2020-05-27 11:51:28 +0900

    ヘッダーデザインの作成

--- laravel/resources/views/nav.blade.php
+++ laravel/resources/views/nav.blade.php
@@ -1,36 +1,54 @@
-<nav>
-    <a href="/">RECOMEN!</a>
-    @guest
-    <ul>
-        <li>
-            <a href="{{ route('login') }}">Login</a>
-        </li>
-        <li>
-            <a href="{{ route('register') }}">Register</a>
-        </li>
-        <li>
-            <a href="{{ route('tags.search') }}">Tag Search</a>
-        </li>
-    </ul>
-    @endguest
-    
-    @auth
-    <ul>
-         <li>
-            <a href="{{ route('products.create') }}">Post</a>
-        </li>
-        <li>
-            <a href="javascript:logout.submit()">Logout</a>
-        </li>
-         <li>
-            <a href="{{ route('users.show', ['name' => Auth::user()->name] )}}">Mypage</a>
-        </li>
-         <li>
-         <a href="{{ route('tags.search') }}">Tag Search</a>
-        </li>
-        <form name="logout" method="POST" action="{{ route('logout') }}" >
-            @csrf
-        </form>
-    </ul>
-    @endauth
-</nav>
+<header class="l-header">
+    <nav class="l-row l-row--between l-row--middle">
+        <div class="p-header--logo">
+            <a href="/">RECOMEN!</a>
+        </div>
+        @guest
+        <div class="c-menu--trigger js-toggle-sp-menu">
+            <span></span>
+            <span></span>
+            <span></span>
+        </div>
+        <div class="c-menu--list js-toggle-sp-menu-target">
+            <ul class="c-list">
+                <li>
+                    <a href="{{ route('login') }}">Login</a>
+                </li>
+                <li>
+                    <a href="{{ route('register') }}">Register</a>
+                </li>
+                <li>
+                    <a href="{{ route('tags.search') }}">Tag Search</a>
+                </li>
+            </ul>
+        </div>
+        @endguest
+        
+        @auth
+         <div class="c-menu--trigger js-toggle-sp-menu">
+            <span></span>
+            <span></span>
+            <span></span>
+        </div>
+        <div class="c-menu--list js-toggle-sp-menu-target">
+            <ul class="c-list">
+                <li>
+                    <a href="{{ route('products.create') }}">Post</a>
+                </li>
+                <li>
+                    <a href="javascript:logout.submit()">Logout</a>
+                </li>
+                <li>
+                    <a href="{{ route('users.show', ['name' => Auth::user()->name] )}}">Mypage</a>
+                </li>
+                <li>
+                <a href="{{ route('tags.search') }}">Tag Search</a>
+                </li>
+                <form name="logout" method="POST" action="{{ route('logout') }}" >
+                    @csrf
+                </form>
+            </ul>
+        </div>
+        @endauth
+    </nav>
+</header>
